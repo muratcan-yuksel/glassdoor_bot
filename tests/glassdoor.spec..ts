70f1e0340911396e57f52ec497f42a3c02075e0a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-01-23 05:50:30 +0300

    get all job links

--- tests/glassdoor.spec..ts
+++ tests/glassdoor.spec..ts
@@ -1,6 +1,7 @@
 import { test, expect, firefox } from "@playwright/test";
 require("dotenv").config();
 const fs = require("fs");
+const { setTimeout } = require("timers");
 
 test.describe("Logging in", () => {
   test("glassdoor login", async ({}) => {
@@ -36,7 +37,7 @@ test.describe("Logging in", () => {
 });
 
 test.describe("navigation", () => {
-  test("glassdoor navigation", async ({}) => {
+  test("open main page", async ({}) => {
     const browser = await firefox.launch();
     const context = await browser.newContext();
 
@@ -50,4 +51,69 @@ test.describe("navigation", () => {
 
     // await browser.close();
   });
+  test("open jobs page", async ({}) => {
+    const browser = await firefox.launch();
+    const context = await browser.newContext();
+
+    const cookies = fs.readFileSync("cookies.json", "utf8");
+
+    const deserializedCookies = JSON.parse(cookies);
+    await context.addCookies(deserializedCookies);
+
+    const page = await context.newPage();
+    await page.goto("https://www.glassdoor.com");
+    //write to location bar
+    await page
+      .locator('[data-test="search-bar-location-input"]')
+      .fill("remote");
+    //click on location bar
+    await page.getByText("Remote (Work From Home), US").click();
+
+    //write to jobs bar
+    await page
+      .locator('[data-test="search-bar-keyword-input"]')
+      .fill("full stack web developer");
+    //click on jobs bar
+    await page
+      .getByRole("listitem")
+      .filter({ hasText: "Full Stack Web Developer Jobs" })
+      .locator("div")
+      .nth(2)
+      .click();
+
+    //close annoying modal
+    await page.locator('[data-test="modal-jobalert"] svg').click();
+
+    //wait for the page to load
+    await page.waitForLoadState();
+
+    //configuring job search
+    //click "more" button
+    await page.locator('[data-test="more-filter"]').getByText("More").click();
+    //easy apply only
+    await page.locator('[data-test="filterAPPLICATION_TYPE"] label').click();
+    //wait for it
+    await page
+      .locator('[data-test="filterAPPLICATION_TYPE"] label')
+      .isChecked();
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    //work from home only
+    await page.locator('[data-test="filterREMOTE_WORK_TYPE"] label').click();
+    //click "more" button again to close it
+    await page.locator('[data-test="more-filter"]').getByText("More").click();
+    //get all job links
+    const links = await page.evaluate(() => {
+      const element = document.querySelector("#MainCol");
+      const links = Array.from(element.querySelectorAll(".react-job-listing"));
+      return links.map((el) => el.querySelector("a").getAttribute("href"));
+    });
+    console.log(links);
+  });
+  // locator('[data-test="MainColSummary"]')
+  // getByRole("listitem")
+  //   .filter({
+  //     hasText:
+  //       "One Stop Swap, Inc.Full Stack Web DeveloperRemote$20.00 Per Hour(Employer est.)E",
+  //   })
+  //   .locator('[data-test="job-link"]');
 });
